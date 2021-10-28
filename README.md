7f1781200c4dcb1a50ab2f22cd2ba077dc50f76d
yadukrishnakp <yadukrishnakp007@gmail.com>
2021-10-28 18:06:54 +0530

    new feature is added which used to find populations in specific cities at UK

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Web scraping
-this is a web scraping program which used for extracting data of cities and populations in there. 
-
+* this is a web scraping program which used for extracting data of cities and populations in there. 
+* there is an option for search populations in cities
 ### Installation
 pip3 install -r requirements.txt
 ### For run
--- app.py
+++ app.py
@@ -7,13 +7,36 @@ result = requests.get(url)
 soup = BeautifulSoup(result.text, 'html.parser')
 
 # creating beautifulsoup object
-datas = soup.find('tbody', {'class': 'jsx-1911055634'})
+soup_data = soup.find('tbody', {'class': 'jsx-1911055634'})
 
 # storing data into file
 output_file = open('UK.txt', 'w')
 
+
 # printing data of cities and populations in there
-for data in datas:
-    title = data.find_all('td')
-    print('city - ' + title[0].text, ' , '+'population - ' + title[1].text)
-    output_file.write('city - ' + title[0].text + ' , ' + 'population - ' + title[1].text + '\n\n')
+def find_all_datas():
+    for sdata in soup_data:
+        title = sdata.find_all('td')
+        print('city - ' + title[0].text, ' , ' + 'population - ' + title[1].text)
+        output_file.write('city - ' + title[0].text + ' , ' + 'population - ' + title[1].text + '\n\n')
+
+
+# for find population in cities
+def search_population(city_name):
+    for data in soup_data:
+        city = data.find_all('td')[0].text
+        population = data.find_all('td')[1].text
+        if any(s.lower() in city.lower() for s in city_name):
+            print(city, ',' + 'population -' + ' ' + population)
+
+
+# creating objects
+
+# can add city name in list for find population
+sort = ['london', 'liverpool']
+
+# get all data from website
+search_population(sort)
+
+# used for search population in cities
+find_all_datas()
